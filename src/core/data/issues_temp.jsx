89cf1ba2a52ee1bd13f7586530ce625c91a50527
Stone Tech <jsreputation@gmail.com>
2022-01-14 01:45:16 +0900

    add: navigation to detailed issue page by id

--- src/core/data/issues_temp.jsx
+++ src/core/data/issues_temp.jsx
@@ -3,6 +3,7 @@ export const tempIssues = [
         id: 1,
         background: 'https://d14zl4he2x0kj1.cloudfront.net/properties_images/property_types/multi_family.jpg',
         title: 'Multi-family Development',
+        description: 'The description about Multi-family Development',
         status: 1,
         series: 49
     },
@@ -10,6 +11,7 @@ export const tempIssues = [
         id: 2,
         background: 'https://d14zl4he2x0kj1.cloudfront.net/properties_images/property_types/flip_house.jpg',
         title: 'House Flip',
+        description: 'The description about Multi-family Development',
         status: 0,
         series: 50
     },
@@ -17,6 +19,7 @@ export const tempIssues = [
         id: 3,
         background: 'https://d14zl4he2x0kj1.cloudfront.net/properties_images/property_types/rental_property.jpg',
         title: 'Multi-family Rental',
+        description: 'The description about Multi-family Development',
         status: 2,
         series: 43
     },
@@ -24,6 +27,7 @@ export const tempIssues = [
         id: 4,
         background: 'https://d14zl4he2x0kj1.cloudfront.net/properties_images/property_types/luxury_home.jpg',
         title: 'Luxury Home',
+        description: 'The description about Multi-family Development',
         status: 2,
         series: 28
     }
--- src/core/ui-kit/card/card.jsx
+++ src/core/ui-kit/card/card.jsx
@@ -1,8 +1,14 @@
+import { useRouter } from 'next/router';
 import { Card } from 'antd';
-
+import { EditOutlined } from '@ant-design/icons';
 const { Meta } = Card;
 
 export default function MCard({cards, issue}) {
+   const router = useRouter();
+
+   const navigateToDetailed = (id) => {
+        router.push('/issue/' + id);
+   }
     return (
         <div>
             {
@@ -20,8 +26,12 @@ export default function MCard({cards, issue}) {
                             hoverable
                             style={{width: 240}}
                             cover={<img alt={el.title} src={el.background} />}
+                            actions={[
+                                    <EditOutlined key="edit" onClick={() => navigateToDetailed(el.id)}/>
+                                ]
+                            }
                         >
-                            <Meta title={el.title}/>
+                            <Meta title={el.title} description={el.description}/>
                         </Card>
                     ))
                 )
