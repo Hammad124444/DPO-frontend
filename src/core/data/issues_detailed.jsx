4be47bfc6d555abe21a13736f94f86b60eca5201
Stone Tech <jsreputation@gmail.com>
2022-01-14 02:14:00 +0900

    add: detailed issue examples

--- src/core/data/issues_detailed.jsx
+++ src/core/data/issues_detailed.jsx
@@ -1,3 +1,102 @@
 export const detaildIssueInfo = [
-    
+    {
+        id: 1,
+        background: 'https://d14zl4he2x0kj1.cloudfront.net/properties_images/property_types/multi_family.jpg',
+        title: 'Multi-family Development',
+        description: 'The description about Multi-family Development',
+        status: 1,
+        series: 49,
+        open: true,
+        issuable: true,
+        capreached: true,
+        tminted: 5000,
+        tsold: 1450,
+        fundraised: {
+            eth: 4.5,
+            tokens: {
+                usdt: 2400,
+                dai: 1200,
+                usdc: 3500
+            },
+            cash: 42000
+        },
+        investors: 4,
+        balanceLocked: 15000,
+        balanceUnlocked: 41000
+    },
+    {
+        id: 2,
+        background: 'https://d14zl4he2x0kj1.cloudfront.net/properties_images/property_types/flip_house.jpg',
+        title: 'House Flip',
+        description: 'The description about Multi-family Development',
+        status: 1,
+        series: 49,
+        open: true,
+        issuable: true,
+        capreached: true,
+        tminted: 5000,
+        tsold: 1450,
+        fundraised: {
+            eth: 4.5,
+            tokens: {
+                usdt: 2400,
+                dai: 1200,
+                usdc: 3500
+            },
+            cash: 42000
+        },
+        investors: 4,
+        balanceLocked: 15000,
+        balanceUnlocked: 41000
+    },
+    {
+        id: 3,
+        background: 'https://d14zl4he2x0kj1.cloudfront.net/properties_images/property_types/rental_property.jpg',
+        title: 'Multi-family Rental',
+        description: 'The description about Multi-family Development',
+        status: 1,
+        series: 49,
+        open: true,
+        issuable: true,
+        capreached: true,
+        tminted: 5000,
+        tsold: 1450,
+        fundraised: {
+            eth: 4.5,
+            tokens: {
+                usdt: 2400,
+                dai: 1200,
+                usdc: 3500
+            },
+            cash: 42000
+        },
+        investors: 4,
+        balanceLocked: 15000,
+        balanceUnlocked: 41000
+    },
+    {
+        id: 4,
+        background: 'https://d14zl4he2x0kj1.cloudfront.net/properties_images/property_types/luxury_home.jpg',
+        title: 'Luxury Home',
+        description: 'The description about Multi-family Development',
+        status: 1,
+        series: 49,
+        open: true,
+        issuable: true,
+        capreached: true,
+        tminted: 5000,
+        tsold: 1450,
+        fundraised: {
+            eth: 4.5,
+            tokens: {
+                usdt: 2400,
+                dai: 1200,
+                usdc: 3500
+            },
+            cash: 42000
+        },
+        investors: 4,
+        balanceLocked: 15000,
+        balanceUnlocked: 41000
+    }
 ]
--- src/pages/issue/[issueId].jsx
+++ src/pages/issue/[issueId].jsx
@@ -1,6 +1,6 @@
 
-export default function MIssuesById() {
+export default function MIssuesById({params}) {
     return(
-        <h1>Show issue cards by Id</h1>
+        <h1>Show issue cards by Id: {params.id}</h1>
     )
 }
