f9083a1e37060e4a9cc4061c3ba377ca3f423e55
Stone Tech <jsreputation@gmail.com>
2022-01-15 10:02:36 +0900

    add: minor detailed issue page start

--- src/pages/issue/[issueId].jsx
+++ src/pages/issue/[issueId].jsx
@@ -1,7 +1,23 @@
 import {  } from 'antd';
+import Image from 'next/image';
+import { detaildIssueInfo } from '../../core/data/issues_detailed';
 
 export default function MIssuesById() {
+    const exampleData = detaildIssueInfo[0];
     return(
-                
+        <div className="container">
+        {
+            exampleData ? (
+                <div>
+                    <Image src={exampleData.background} 
+                        alt={exampleData.title}
+                    />
+                    <p>{exampleData.description}</p>
+                </div>
+            ) : (
+                <h1>Empty</h1>
+            )
+        }
+         </div>
     )
 }
