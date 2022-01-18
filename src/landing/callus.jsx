12c8d723ec83a0426917d234061a78ea85125622
jsreputation <jsreputation@gmail.com>
2022-01-19 02:01:37 +0800

    add: whychooseus, options and colors styles

---
+++ src/landing/callus.jsx
@@ -0,0 +1,5 @@
+export default function MLandingCallUs() {
+    return(
+        <h1>Call Us Component</h1>
+    )
+}
--- src/landing/content.jsx
+++ src/landing/content.jsx
@@ -1,37 +1,17 @@
-import { Card, Button } from 'antd';
-import { BulbOutlined, DollarOutlined, RocketOutlined } from '@ant-design/icons';
 import MCarousel from "../core/ui-kit/carousel/carousel";
+import MLandingOptions from "./options";
+import MLandingWhyChoose from "./whychoose";
 import {carousels} from "../core/data/carousels";
 import {LandingCards} from "../core/data/landingcards";
-import MPrimaryBtn from "../core/ui-kit/buttons/primaryBtn";
+import {WhyChooseUs} from "../core/data/landing/chooseus";
 
-const { Meta } = Card;
 
 export default function MLandingContent() {
     return(
         <div className="container-fluid no-padding">
             <MCarousel carousels={carousels}/>
-            <div className="ant-row container pt-30">
-                {
-                    LandingCards.map((el) => (
-                        <div key={el.id} className="ant-col-8 ant-col-sm-8 ant-col-xs-24">
-                            <Card
-                                className="pt-30"
-                                cover={<BulbOutlined style={{ fontSize: '50px'}} />}
-                                actions={[
-                                    <MPrimaryBtn key={el.id} type="default" label={el.button} size="default"/>
-                                ]}
-                            >
-                                <Meta
-                                    className="text-center"
-                                    title={el.title}
-                                    description={el.description}
-                                />
-                            </Card>
-                        </div>
-                    ))
-                }
-            </div>
+            <MLandingOptions cards={LandingCards}/>
+            <MLandingWhyChoose detail={WhyChooseUs}/>
         </div>
 
     )
