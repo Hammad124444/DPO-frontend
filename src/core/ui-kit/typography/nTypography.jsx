9eecaa0fa44cedf63da47f0a4062f7385f61a8b7
Stone Tech <jsreputation@gmail.com>
2022-02-17 11:59:10 +0900

    complete: Terms & Conditions

--- src/core/ui-kit/typography/nTypography.jsx
+++ src/core/ui-kit/typography/nTypography.jsx
@@ -2,20 +2,21 @@ import { Typography } from "antd";
 
 const { Title, Paragraph } = Typography;
 
-export default function MKNTypography({ title, content, color, htmlType, titleLevel, contentFont }) {
+export default function MKNTypography({ title, contents, color, htmlType, titleLevel, contentFont }) {
     return(
         <Typography>
             <Title className={'font-'+ titleLevel+ ' ' +color} >{ title }</Title>
             {
-                htmlType ? (
-                    <Paragraph className={'font-'+  contentFont + ' ' + color}>
-                        <div dangerouslySetInnerHTML={{ __html: content }} />
-                    </Paragraph>
-                    ):(
-                    <Paragraph className={'font-'+  contentFont + ' ' + color}>{ content }</Paragraph>
-                )
+                 contents && contents.map((el, index) => (
+                     htmlType ? (
+                         <Paragraph key={index} className={'font-'+  contentFont + ' ' + color}>
+                             <div dangerouslySetInnerHTML={{ __html: el }} />
+                         </Paragraph>
+                         ):(
+                         <Paragraph className={'font-'+  contentFont + ' ' + color}>{ el }</Paragraph>
+                     )
+                 ))
             }
-            
         </Typography>
     )
 }
--- src/staticInfo/termsOfUse.jsx
+++ src/staticInfo/termsOfUse.jsx
@@ -6,9 +6,8 @@ export default function MPTermsOfUse() {
         <div className="container pt-50">
             <h1 className="text-center font-40 font-bold c-green">Terms Of Use</h1>
             {
-                termsOfUse.map((el) => (
-                    !el.type &&
-                        <MKNTypography title={el.title} content={el.description} htmlType titleLevel='24' contentFont={'16'}/>
+                termsOfUse.map((el, index) => (
+                        <MKNTypography key={index} title={el.title} contents={el.descriptions} htmlType titleLevel='24' contentFont={'16'}/>
                 ))
             }
         </div>
